08db972c27b9a60277c05ee131fffe4d6c201fa0
Aaron Wright <aaron.c.wright@icloud.com>
2019-06-05 13:33:09 -0500

    Initial Commit

---
+++ JWT/JWT.h
@@ -0,0 +1,19 @@
+//
+//  JWT.h
+//  JWT
+//
+//  Created by Aaron Wright on 6/5/19.
+//  Copyright © 2019 Infinite Token. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for JWT.
+FOUNDATION_EXPORT double JWTVersionNumber;
+
+//! Project version string for JWT.
+FOUNDATION_EXPORT const unsigned char JWTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JWT/PublicHeader.h>
+
+
